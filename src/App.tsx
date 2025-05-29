7c65377aebe85d349ab21ad907e636c446d1474f
Your Name <user@example.com>
2025-05-29 07:39:10 +0530

    feat: initialize React project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-dvw h-dvh flex flex-col justify-center items-center gap-10">
+      <h1 className="text-4xl">App</h1>
+    </div>
+  );
+};
+
+export default App;
